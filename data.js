e51f6058a5c599dbc21b56de35317dee1e1df4f3
Leonardo.Selmini <selminileonardo@gmail.com>
2021-11-23 18:52:00 +0100

    select for type

--- data.js
+++ data.js
@@ -113,16 +113,77 @@ const icons = [
 	}
 ];
 
-const main = document.querySelector('main');
+select();
 
-// creo per ogni elemento dell'array una card
-icons.forEach((element) => {
-	main.innerHTML += `
-		<div class="card">
-			<div class="icon-container">
-				<i class="${element.family} ${element.prefix}${element.name}"></i>
-				<span class="icon-label">${element.name}</span>
-			</div>
-		</div>
-	`
-});
+// 00000000 FUNZIONI 00000000
+
+function select() {
+	const main = document.querySelector('main');
+	const mySelect = document.getElementById('type-select').value;
+
+	switch (mySelect) {
+		case 'animals':
+			console.log('animals');
+			main.innerHTML = '';
+			icons.filter((element) => {
+				if (element.type == 'animal') {
+					main.innerHTML += `
+					<div class="card">
+						<div class="icon-container">
+							<i class="${element.family} ${element.prefix}${element.name}"></i>
+							<span class="icon-label">${element.name}</span>
+						</div>
+					</div>`
+				}
+			});
+			break;
+
+		case 'fruits':
+			console.log('fruit');
+			main.innerHTML = '';
+			icons.filter((element) => {
+				if (element.type == 'fruit') {
+				main.innerHTML += `
+				<div class="card">
+					<div class="icon-container">
+						<i class="${element.family} ${element.prefix}${element.name}"></i>
+						<span class="icon-label">${element.name}</span>
+					</div>
+				</div>`
+				}
+			});
+			break;
+
+		case 'people':
+			console.log('people');
+			main.innerHTML = '';
+			icons.filter((element) => {
+				if (element.type == 'user') {
+				main.innerHTML += `
+				<div class="card">
+					<div class="icon-container">
+						<i class="${element.family} ${element.prefix}${element.name}"></i>
+						<span class="icon-label">${element.name}</span>
+					</div>
+				</div>`
+				}
+			});
+			break;
+
+		default:
+			console.log('all');
+			main.innerHTML = '';
+			icons.forEach((element) => {
+				main.innerHTML += `
+					<div class="card">
+						<div class="icon-container">
+							<i class="${element.family} ${element.prefix}${element.name}"></i>
+							<span class="icon-label">${element.name}</span>
+						</div>
+					</div>
+				`
+			});
+			break;
+	}
+}
+
